463d2c9aebb604a26ab1dab29726b6a1110b9ab2
svyatoslav-reshetnikov <svyatoslav.reshetnikov@gmail.com>
2016-08-05 13:30:30 +0300

    Initial commit

---
+++ VirtualTourist/PhotoAlbumViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by SVYAT on 05.08.16.
+//  Copyright © 2016 HiT2B. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ VirtualTourist/TravelLocationMapViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TravelLocationMapViewController.swift
+//  VirtualTourist
+//
+//  Created by SVYAT on 04.08.16.
+//  Copyright © 2016 HiT2B. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationMapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
